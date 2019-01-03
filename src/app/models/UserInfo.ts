e86b7fc7fb7e7ee3f153663f98091be533f486ae
zackigoldberg <zackigoldberg@gmail.com>
2019-01-03 11:53:02 -0500

    added delete

--- src/app/models/UserInfo.ts
+++ src/app/models/UserInfo.ts
@@ -1,5 +1,4 @@
 export interface UserInfo {
     Email: string;
-    HasRegisterd: boolean;
-    LoginProvider?: any;
+    password: string;
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Token } from '../models/Token';
+import { UserInfo} from '../models/UserInfo';
 import { Router } from '@angular/router';
-import { Observable,Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 
 const Api_Url = "https://efa-gardenapp-backend.herokuapp.com/api/auth/login";
@@ -11,34 +12,27 @@ const Api_Url = "https://efa-gardenapp-backend.herokuapp.com/api/auth/login";
 export class AuthService {
   userInfo: Token;
   isLoggedIn = new Subject<boolean>();
+  loginInfo: UserInfo;
+
   constructor(private _http: HttpClient, private _router: Router) {
-    
   }
 
   login(loginInfo) {
     const str =
-    `grant_type=password&username=${encodeURI(loginInfo.email)}&password=${encodeURI(loginInfo.password)}`;
+    `grant_type=password&email=${encodeURI(loginInfo.email)}&password=${encodeURI(loginInfo.password)}`;
 
-    return this._http.post(`${Api_Url}/token`, str).subscribe( (token: Token) => {
+    return this._http.post(`${Api_Url}`, str).subscribe( (token: Token) => {
       this.userInfo = token;
-      localStorage.setItem('id_token', token.access_token);
+      localStorage.setItem('token', token.token);
       this.isLoggedIn.next(true);
       this._router.navigate(['/']);
     });}
-
-    currentUser(): Observable<Object> {
-      if (!localStorage.getItem('id_token')) {return new Observable(observer => observer.next(false)); }
-
-      const authHeader = new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('id_token')}`);
-
-      return this._http.get(`${Api_Url}/api/Account/UserInfo`, {headers: authHeader});
-    }
 
     logout() {
       localStorage.clear();
       this.isLoggedIn.next(false);
 
-      const authHeader= new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('id_token')}`);
+      const authHeader= new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('token')}`);
 
       this._http.post(`${Api_Url}/api/Account/Logout`, { headers: authHeader});
       this._router.navigate(['/login']);
